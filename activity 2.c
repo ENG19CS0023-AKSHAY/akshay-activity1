cb9fb9137e0ea0c638a373aa3e4e16c20ea2e54d
ENG19CS0023-AKSHAY <76054098+ENG19CS0023-AKSHAY@users.noreply.github.com>
2021-02-04 09:29:17 +0530

    Update activity 2.c

--- activity 2.c
+++ activity 2.c
@@ -1,1 +1,11 @@
 //Write a program to add two user input numbers using one function.
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers\n");
+    scanf("%d%d",&a,&b);
+    printf("%d+%d=%d",a,b,(a+b));
+    return 0;
+}
+
