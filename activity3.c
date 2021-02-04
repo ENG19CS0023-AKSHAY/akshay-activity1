1335128d56002a44583c2155b49d888b02313f98
ENG19CS0023-AKSHAY <76054098+ENG19CS0023-AKSHAY@users.noreply.github.com>
2021-02-04 09:55:04 +0530

    Update activity3.c

--- activity3.c
+++ activity3.c
@@ -1,1 +1,11 @@
 //Write a program to find the volume of a tromboloid using one function
+#include<stdio.h>
+int main()
+{
+    float h,d,b,volume;
+    printf("Enter height,breadth and depth\n");
+    scanf("%f%f%f",&h,&d,&b);
+    volume=((h*d*b)+(d/b))/3.0;
+    printf("Volume=%f",volume);
+    return 0;
+}
