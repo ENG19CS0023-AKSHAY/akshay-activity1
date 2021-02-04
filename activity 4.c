5a532395da43273818c2189c5798559ad78b090f
ENG19CS0023-AKSHAY <76054098+ENG19CS0023-AKSHAY@users.noreply.github.com>
2021-02-04 10:42:37 +0530

    Update activity 4.c

--- activity 4.c
+++ activity 4.c
@@ -1,22 +1,31 @@
 //Write a program to add two user input numbers using 4 functions.
-#include<stdio.h>
-int a,b,sum;
-void input()
+#include <stdio.h>
+int input()
 {
-     printf("Enter the numbers\n");
-     scanf("%d%d",&a,&b);
+    int a; 
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    return a;
 }
-void compute()
+
+int find_sum(int a, int b)
 {
-     sum=a+b;
+    int sum;
+    sum = a+b;
+    return sum;
 }
-void output ()
+
+void output(int a, int b, int c)
 {
-     printf("Sum=%d",sum);
+    printf("Sum of %d + %d is %d\n",a,b,c);
 }
-void main()
+
+int main()
 {
-input();
-compute();
-output();
+    int x,y,z;
+    x=input();
+    y=input();
+    z=find_sum(x,y);
+    output(x,y,z);
+    return 0;
 }
