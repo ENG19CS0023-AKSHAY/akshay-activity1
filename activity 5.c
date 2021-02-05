a6902d171ebe69cdea47d30dba1e4c3cac5d7ff9
ENG19CS0023-AKSHAY <76054098+ENG19CS0023-AKSHAY@users.noreply.github.com>
2021-02-05 18:24:22 +0530

    Update activity 5.c

--- activity 5.c
+++ activity 5.c
@@ -1,1 +1,63 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include <stdio.h>
+
+float input()
+
+{
+
+    float a; 
+
+    scanf("%f",&a);
+
+    return a;
+
+}
+
+
+float find_vol(float a,float b,float c)
+
+{
+
+    float vol;
+
+    vol = ((a*b*c)+(b/c))/3.0;
+
+    return vol;
+
+}
+
+
+void output(float a)
+
+{
+
+    printf("The result is %f",a);
+
+}
+
+
+int main()
+
+{
+
+    float x,y,z,vol;
+    
+    printf("Enter the height\n");
+
+    x=input();
+
+    printf("Enter the depth\n");
+
+    y=input();
+
+    printf("Enter the breadth\n");
+
+    z=input();
+
+    vol=find_vol(x,y,z);
+
+    output(vol);
+
+    return 0;
+
+}
