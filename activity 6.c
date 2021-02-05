a934ab8db01897c4f47951e7ed5384bb6dc2b7da
ENG19CS0023-AKSHAY <76054098+ENG19CS0023-AKSHAY@users.noreply.github.com>
2021-02-05 21:57:05 +0530

    Update activity 6.c

--- activity 6.c
+++ activity 6.c
@@ -16,7 +16,7 @@ float input()
 }
 
 
-float find_distance(float x1, float x2 ,float y1,float y2)
+float find_distance(float x1, float y1 ,float x2,float y2)
 
 {
 
@@ -29,41 +29,28 @@ float find_distance(float x1, float x2 ,float y1,float y2)
 }
 
 
-void output(float a)
+void output(float a,float b,float c,float d,float e)
 
 {
 
-    printf("The result is %f\n”,a);
-
+    printf("The distance between (%f,%f) and (%f,%f) is %f\n”,b,c,d,e,a);
 }
-
-
 int main()
-
 {
-
-    float x,y,x1,y1;
+    float x,x1,y,y1;
     
     printf("Enter X coordinate\n");
     
     x=input();
-
     printf("Enter Y coordinate\n");
-
     y=input();
     
     printf("Enter X1 coordinate\n");
-
     x1=input();
-
     printf("Enter Y1 coordinate\n");
-
     y1=input();
-
-    z=find_distance(x,x1,y,y1);
-
-    output(z);
-
+    z=find_distance(x,y,x1,y1);
+    output(z,x,y,x1,y1);
     return 0;
-
 }
+
