36817706ef871b513db4ad662c7c820be9c47d88
ENG19CS0023-AKSHAY <76054098+ENG19CS0023-AKSHAY@users.noreply.github.com>
2021-02-05 18:47:47 +0530

    Update activity 7.c

--- activity 7.c
+++ activity 7.c
@@ -1,1 +1,73 @@
 //WAP to find the distance between two points using structures and 4 functions.
+#include <stdio.h>
+
+#include <math.h>
+
+struct point
+
+{
+
+    float x;
+
+    float y;
+
+}Point1;
+
+Point1 input()
+
+{
+
+    Point1 p;
+
+    printf("Enter abcissa ");
+
+    scanf("%f",&p.x);
+
+    printf("Enter ordinate ");
+
+    scanf("%f",&p.y);
+
+    return p;
+
+}
+
+float compute(Point1 p1,Point1 p2)
+
+{
+
+    float distance;
+
+    distance=sqrt(pow((p1.x-p2.x),2)+pow((p1.y-p2.y),2));
+
+    return distance;
+
+}
+
+void output(Point1 p1, Point1 p2, float dist)
+
+{
+
+    printf("The distance between %f,%f and %f,%f is %f",p1.x,p1,y,p2.x,p2,y,dist);
+
+}
+
+int main(void)
+
+{
+
+    float dist;
+
+    Point1 p1,p2;
+
+    p1=input();
+
+    p2=input();
+
+    dist=compute(p1,p2);
+
+    output(dist);
+
+    return 0;
+
+}
+
