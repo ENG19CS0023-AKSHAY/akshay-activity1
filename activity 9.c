1c3aa3501ed7df712e1ff220d3a27160fb8fc398
ENG19CS0023-AKSHAY <76054098+ENG19CS0023-AKSHAY@users.noreply.github.com>
2021-02-06 13:22:50 +0530

    Update activity 9.c

--- activity 9.c
+++ activity 9.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int n,sum=0;
-printf(“Enter the total number of numbers\n”);
-scanf(“%d”,&n);
-for(int i=1;i<=n;i++)
-sum=sum+i;
-printf(“The final sum is %d\n”,sum);
-return 0;
+    int n,a[],sum=0;
+    printf(“Enter the total number of numbers\n”);
+    scanf(“%d”,&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter a new number\n");
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }
+    printf(“The final sum is %d\n”,sum);
+    return 0;
 }
+
